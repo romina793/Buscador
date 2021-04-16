bcf825dccbb619ab8dcf4cc7ea88f9b5a7af8a98
romina793 <rominapozzuto@gmail.com>
2021-04-15 22:55:52 -0300

    init project

---
+++ Buscardor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Buscardor
+//
+//  Created by Romina Pozzuto on 15/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
